3f648e26df7649bae8a5754f04046df0ebd5575e
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-15 20:54:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,37 +24,4 @@ iR25RR.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
-iR25RR.on('message', message => {
-    if (message.content.split(' ')[0] == '.') {
-        if (message.guild === null) {
-            return;
-        }
-        console.log("used");
-
-
-
-
-        message.guild.members.forEach(member => {
-            setInterval(function () {
-
-
-   member.sendMessage(` 
-**
-السلام عليكم :slight_smile: 
-
-سيرفر بيع وشراء ثقة  :link: 
-سيرفر قيف اوايات يوميه 
-وفيه  تصويت على 
-100K Credit ProBot
-
-رابط السيرفر** https://discord.gg/4kt8cKq
-
-   
-   `);
-            })
-        }, 500);
-    }
-
-});
-
 iR25RR.login(process.env.BOT_TOKEN);
