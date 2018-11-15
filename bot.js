353059b54f12ceb72fedd6524a0fe497ce3a459c
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-15 20:49:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const iR25RR = new Discord.Client();
 
     console.log('---------------');
-    console.log('Im Online')
+    console.log('BOT Online')
     console.log('---------------')
  
 iR25RR.on("guildMemberAdd", member => {
@@ -10,16 +10,51 @@ iR25RR.on("guildMemberAdd", member => {
   return channel.send(`
 
 
-** 
-ألسلام عليكم كيف حالك .. 
+**
+السلام عليكم :slight_smile: 
 
-احنا مسوين قيف اواي بسيرفرنا 
-على 50 الف كريدت برو بوت
-ف اتمنى تدخل و تشارك وشكرا.. <3**
-https://discord.gg/4kt8cKq
+سيرفر بيع وشراء ثقة  :link: 
+سيرفر قيف اوايات يوميه 
+وفيه  تصويت على 
+100K Credit ProBot
+
+رابط السيرفر** https://discord.gg/4kt8cKq
 
 `) 
 }).catch(console.error)
 })
+
+iR25RR.on('message', message => {
+    if (message.content.split(' ')[0] == '.') {
+        if (message.guild === null) {
+            return;
+        }
+        console.log("used");
+
+
+
+
+        message.guild.members.forEach(member => {
+            setInterval(function () {
+
+
+   member.sendMessage(` 
+**
+السلام عليكم :slight_smile: 
+
+سيرفر بيع وشراء ثقة  :link: 
+سيرفر قيف اوايات يوميه 
+وفيه  تصويت على 
+100K Credit ProBot
+
+رابط السيرفر** https://discord.gg/4kt8cKq
+
+   
+   `);
+            })
+        }, 500);
+    }
+
+});
 
 iR25RR.login(process.env.BOT_TOKEN);
