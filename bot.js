54d84a52afcc57664fa7dfd285b1cab0884afb9d
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-15 13:11:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const iR25RR = new Discord.Client();
 
     console.log('---------------');
-    console.log(' Bot Is Online')
+    console.log('Im Online')
     console.log('---------------')
  
 iR25RR.on("guildMemberAdd", member => {
@@ -10,22 +10,16 @@ iR25RR.on("guildMemberAdd", member => {
   return channel.send(`
 
 
-  **
-  السلام عليكم كيف حالك
-  تعريف عن سيرفرنا 
-  سيرفر عبارة عن بيع و شراء  ثقه
-  
-  نبيع اشياء مقابل كريدت
-  و نبيع اشياء مقابل سوا باي بال زين موبايلي
-  اهم شي مايكون غالي
-  
-  قيف اواي على 10 حسابات  Spotify Accounts  
+** 
+ألسلام عليكم كيف حالك .. 
 
-  فتحنا التقديم على رتبة بائع 
-  نستقبل البائعين  **
-  رابط https://discord.gg/R66G6Bg
+احنا مسوين قيف اواي بسيرفرنا 
+على 50 الف كريدت برو بوت
+ف اتمنى تدخل و تشارك وشكرا.. <3**
+https://discord.gg/4kt8cKq
 
 `) 
 }).catch(console.error)
 })
+
 iR25RR.login(process.env.BOT_TOKEN);
