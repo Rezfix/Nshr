93b40eab1fb3b3e3dc85e4e30bd2a2833be4d625
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-15 20:55:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const iR25RR = new Discord.Client();
 
     console.log('---------------');
-    console.log('BOT Online')
+    console.log('BOT IS Online')
     console.log('---------------')
  
 iR25RR.on("guildMemberAdd", member => {
